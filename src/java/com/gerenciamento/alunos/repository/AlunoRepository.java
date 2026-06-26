f91c920aa27922b01ec0603970e69a657173b97d
Carlos Henrique <ch08082003@gmail.com>
2026-06-26 19:09:42 -0300

    Create AlunoRepository.java

---
+++ src/java/com/gerenciamento/alunos/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.gerenciamento.alunos.repository;
+
+import com.gerenciamento.alunos.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
