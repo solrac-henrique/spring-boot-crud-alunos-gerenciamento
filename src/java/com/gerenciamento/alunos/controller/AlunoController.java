659208784e0ce99da40723c543b64048a048af39
Carlos Henrique <ch08082003@gmail.com>
2026-06-26 19:10:42 -0300

    Create AlunoController.java

---
+++ src/java/com/gerenciamento/alunos/controller/AlunoController.java
@@ -0,0 +1,42 @@
+package com.gerenciamento.alunos.controller;
+
+import com.gerenciamento.alunos.model.Aluno;
+import com.gerenciamento.alunos.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/")
+public class AlunoController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping
+    public String listarAlunos(Model model) {
+        model.addAttribute("alunos", alunoRepository.findAll());
+        return "index";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarAluno(@ModelAttribute Aluno aluno) {
+        alunoRepository.save(aluno);
+        return "redirect:/";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluirAluno(@PathVariable Long id) {
+        alunoRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarAluno(@PathVariable Long id, Model model) {
+        Aluno aluno = alunoRepository.findById(id).orElse(null);
+        model.addAttribute("aluno", aluno);
+        model.addAttribute("alunos", alunoRepository.findAll());
+        return "index";
+    }
+}
